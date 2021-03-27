dd375ea3110e58ab36f3838f45dc014e7add0dd9
Yuri <dog-pit10@yandex.ru>
2021-03-27 14:34:46 +0300

    added @media

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import c from "./Works.module.css"
 import todo from "../../assets/todoList.png"
+import diet from "../../assets/diet.png"
+import simple from "../../assets/simple.png"
 
 export function Works() {
 
@@ -20,14 +22,42 @@ export function Works() {
                     </div>
                 </div>
             </div>
-            <div className='container'>
+            <div className={c.container}>
+
                 <div className={c.work_todo}>
-                    <a className='text-decoration-none' href="https://yuri1011.github.io/tasklist/" target='_blank'>
-                        <p className={c.title_todo}>
-                            To-do list
-                        </p>
-                        <img src={todo} alt='image_todo_list' className='borderWorksImg card-img w-25 rounded'/>
-                    </a>
+                    <div id={'img'}>
+                        <div>
+                            <a className='text-decoration-none' href="https://yuri1011.github.io/tasklist/"
+                               target='_blank'>
+                                <p className={c.title_todo}>To-do list</p>
+                            </a>
+                        </div>
+                        <div>
+                            <img src={todo} alt='image_todo_list' className='borderWorksImg card-img w-75 rounded'/>
+                        </div>
+                    </div>
+                    <div id={'img'}>
+                        <div>
+                            <a className='text-decoration-none' href="https://yuri1011.github.io/Diet/"
+                               target='_blank'>
+                                <p className={c.title_todo}>Diet</p>
+                            </a>
+                        </div>
+                        <div>
+                            <img src={diet} alt='image_todo_list' className='borderWorksImg card-img w-75 rounded'/>
+                        </div>
+                    </div>
+                    <div id={'img'}>
+                        <div>
+                            <a className='text-decoration-none' href="https://yuri1011.github.io/Simple/"
+                               target='_blank'>
+                                <p className={c.title_todo}>Simple</p>
+                            </a>
+                        </div>
+                        <div>
+                            <img src={simple} alt='image_todo_list' className='borderWorksImg card-img w-75 rounded'/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
